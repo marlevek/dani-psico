0d8ad62f0768dc0c9616e5ffaab2ca6a12052e3c
Marcelo <marcelo_levek@hotmail.com>
2023-01-21 14:00:04 -0300

    Update page-home.php

--- wp-content/themes/tema-dani-terapeuta/page-home.php
+++ wp-content/themes/tema-dani-terapeuta/page-home.php
@@ -1,15 +1,50 @@
 <?php get_header(); ?>
-    
-    <nav class="navbar fixed-top navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand text-primary text-end" href="#section-2">Aqui vão os links do menu</a>
+<nav class="navbar navbar-expand-lg navbar-light ">
+  <div class="container">
+  <a class="navbar-brand" href="#">Espaço Gestalt</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+     
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#section-2">Sobre</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#section-3">Terapias</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#section-4">Contato</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Blog</a>
+        </li>
+      </ul>
+     
+    </div>
   </div>
 </nav>
 
 
 
 
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
   
 <?php get_template_part('home-sections'); ?>
 
