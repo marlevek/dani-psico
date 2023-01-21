d215ef8b61d066941d8148a6ea9e73199cf8be0c
Marcelo <marcelo_levek@hotmail.com>
2023-01-21 12:02:55 -0300

    Update index.php

--- wp-content/themes/tema-dani-terapeuta/index.php
+++ wp-content/themes/tema-dani-terapeuta/index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
     <div class="container">
-        <h1 class="text-center">Página Inicial</h1>
+        <h1 class="text-center text-primary">Página Inicial</h1>
     </div>
     
 <?php get_footer(); ?>
