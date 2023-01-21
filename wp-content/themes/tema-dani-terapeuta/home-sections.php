20c341d69ad2401c7a0f3f40336c0cd2d50aea66
Marcelo <marcelo_levek@hotmail.com>
2023-01-21 13:13:13 -0300

    Update home-sections.php

--- wp-content/themes/tema-dani-terapeuta/home-sections.php
+++ wp-content/themes/tema-dani-terapeuta/home-sections.php
@@ -1,14 +1,14 @@
 <section id="section-1" class="home-section">
-   <h1 class="text-white">OLÁ MUNDO</h1>
+      
 </section>
 
 <section id="section-2" class="home-section">
     <div class="container">
-   <div class="row-justify-content-evenly">
-    <div class="col-6">
-        <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/fot-Danielle.png" alt="foto Danielle Zagonel Machado" alt='foto Danielle Zagonel Machado'>
+   <div class="row justify-content-around">
+    <div class="col-6 mt-5">
+        <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/foto-dani-circular.png" alt="foto Danielle Zagonel Machado" alt='foto Danielle Zagonel Machado'>
     </div>
-    <div class="col-6">
+    <div class="col-6 mt-5">
         <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, quidem quod. Velit, delectus officia. Voluptate adipisci quaerat laboriosam voluptatem ea repudiandae aspernatur, qui vel doloribus at quisquam, iste assumenda quia!</h3>
     </div>
    </div>
