bcb51b95d704c2ee47abca925cf0fa67b3eb7388
Marcelo <marcelo_levek@hotmail.com>
2023-01-21 12:02:52 -0300

    Create home-sections.php

---
+++ wp-content/themes/tema-dani-terapeuta/home-sections.php
@@ -0,0 +1,11 @@
+<section id="section-1" class="home-section">
+   <h1>OLÁ MUNDO</h1>
+</section>
+
+<section id="section-2" class="home-section">
+    <!-- Conteúdo da seção 2 -->
+</section>
+
+<section id="section-3" class="home-section">
+    <!-- Conteúdo da seção 3 -->
+</section>
