06e9c464f0580b96e66bc4e12492a07c0fa36be5
Marcelo <marcelo_levek@hotmail.com>
2023-01-21 13:13:15 -0300

    Update page-home.php

--- wp-content/themes/tema-dani-terapeuta/page-home.php
+++ wp-content/themes/tema-dani-terapeuta/page-home.php
@@ -1,7 +1,16 @@
 <?php get_header(); ?>
-    <div class="container">
+    
+    <nav class="navbar fixed-top navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand text-primary text-end" href="#section-2">Aqui vão os links do menu</a>
+  </div>
+</nav>
+
+
+
+
         
-   </div>
+  
 <?php get_template_part('home-sections'); ?>
 
 
