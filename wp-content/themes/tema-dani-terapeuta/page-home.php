f0b887fa7c6218a28cf08001427361c51dfc92e3
Marcelo <marcelo_levek@hotmail.com>
2023-01-21 12:03:01 -0300

    Create page-home.php

---
+++ wp-content/themes/tema-dani-terapeuta/page-home.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+    <div class="container">
+        <h1 class="text-center text-primary">Página Inicial</h1>
+
+<?php get_template_part('home-sections'); ?>
+
+
+
+
+
+
+
+
+
+
+    </div>
+    
+<?php get_footer(); ?>
