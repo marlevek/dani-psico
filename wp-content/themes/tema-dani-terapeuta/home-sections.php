69b96fb8e0a72d70179b4ab33c2f4691617c3061
Marcelo <marcelo_levek@hotmail.com>
2023-01-24 15:15:19 -0300

    Update home-sections.php

--- wp-content/themes/tema-dani-terapeuta/home-sections.php
+++ wp-content/themes/tema-dani-terapeuta/home-sections.php
@@ -18,12 +18,49 @@
 </div>
 </section>
 
+<div class="container">
 <section id="section-3" class="home-section">
-   <h1>PSICOTERAPIAS</h1>
-
-
-   
+    <h2 class="psicoterapias_tit mt-5">PSICOTERAPIAS</h2>
+<div class="row row-cols-1 row-cols-md-2 g-4 mt-4">
+  <div class="col">
+    <div class="card">
+      <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/teclando-no-notebook.png" class="card-img-top" alt="imagem pessoa teclando no notebook">
+      <div class="card-body">
+        <h5 class="card-title">Psicoterapia à distância</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/imagem-pessoa-feliz.png" class="card-img-top" alt="imagem pessoa feliz">
+      <div class="card-body">
+        <h5 class="card-title">Psicoterapia para adultos</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/imagem-criancas-e-adolescentes.png" class="card-img-top" alt="imagem crianças e adolescentes sentados na grama">
+      <div class="card-body">
+        <h5 class="card-title">Psicoterapia para crianças e adolescentes</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/imagem-pais-e-filho.png" class="card-img-top" alt="imagem pais e filho ao por do sol">
+      <div class="card-body">
+        <h5 class="card-title">Aconselhamento parental</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
 </section>
+</div>
 <section id="section-4" class="home-section">
     <h1>FORMULÁRIO DE CONTATO</h1>
 </section>
