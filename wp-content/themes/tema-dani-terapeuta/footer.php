f80d5869a1d2247d1686e138f017b060c3e84e23
Marcelo <marcelo_levek@hotmail.com>
2023-01-24 16:20:30 -0300

    Update footer.php

--- wp-content/themes/tema-dani-terapeuta/footer.php
+++ wp-content/themes/tema-dani-terapeuta/footer.php
@@ -1,8 +1,23 @@
 <footer>
+    <div class="container-fluid footer-bg mt-5">
+    <div class="container">
+<div class="row justify-content-evenly">
+    <div class="col-4 mt-5">
+      <span class="titulo">Danielle Zagonel Machado</span>
+      <p class="cor_texto">Psicoterapeuta</p>
+    </div>
+    <div class="col-4 mt-5">
+      <p class="cor_texto">&copy; copyright 2023 | <a href="https://www.mzlmarketingdigital.com.br" target="_blank" class="mzl">MZL Marketing Digital</a></p>
+    </div>
+    <div class="col-4 mt-5">
+       <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/logo-email.png" alt="email Danielle Machado">
+       <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/logo-facebook.png" alt="facebook Danielle Machado">
+       <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/logo-whatsapp.png" alt="whatsapp Danielle Machado">
+    </div>
+  </div>
+  </div>
 
-
-
-
+</div>
 
 </footer>
 </body>
