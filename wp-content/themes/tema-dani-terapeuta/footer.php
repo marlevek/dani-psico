d86179a3b6b0afc9c218b449e2b688422f2ea2af
Marcelo <marcelo_levek@hotmail.com>
2023-01-28 11:31:50 -0300

    Update footer.php

--- wp-content/themes/tema-dani-terapeuta/footer.php
+++ wp-content/themes/tema-dani-terapeuta/footer.php
@@ -1,22 +1,21 @@
 <footer>
-    <div class="container-fluid footer-bg mt-5">
+    <div class="container-fluid footer-bg">
     <div class="container">
-<div class="row justify-content-evenly">
-    <div class="col-4 mt-5">
+<div class="row pt-4">
+    <div class="col-sm-12 col-md-6 nome-footer">
       <span class="titulo">Danielle Zagonel Machado</span>
       <p class="cor_texto">Psicoterapeuta</p>
+    </div> 
+    <div class="col-sm-12 col-md-6 contato-footer">
+       <a href="" target="_blank" class=""><img src="http://localhost/dani-psico/wp-content/uploads/2023/01/logo-email.png" alt="imagem e-mail" ></a>
+       <a href="https://www.facebook.com/espacogestalt" target="_blank" class=""><img src="http://localhost/dani-psico/wp-content/uploads/2023/01/logo-facebook.png" alt="facebook Danielle Machado" alt="imagem facebook" ></a>
+       <a href="https://wa.me/5541988416624?text=sua mensagem" target="_blank" class=""><img src="http://localhost/dani-psico/wp-content/uploads/2023/01/imagem-whatsapp-rodape.png" alt="whatsapp Danielle Machado" alt="imagem whatsapp" ></a>
     </div>
-    <div class="col-4 mt-5">
+  </div>
+ <div class="col-sm-12 col-md-12 text-center">
       <p class="cor_texto">&copy; copyright 2023 | <a href="https://www.mzlmarketingdigital.com.br" target="_blank" class="mzl">MZL Marketing Digital</a></p>
     </div>
-    <div class="col-4 mt-5">
-       <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/logo-email.png" alt="email Danielle Machado">
-       <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/logo-facebook.png" alt="facebook Danielle Machado">
-       <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/logo-whatsapp.png" alt="whatsapp Danielle Machado">
-    </div>
   </div>
-  </div>
-
 </div>
 
 </footer>
