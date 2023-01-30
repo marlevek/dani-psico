0f4733455142eb74f356d5b7cbf60730b55aff55
Marcelo <marcelo_levek@hotmail.com>
2023-01-30 08:40:27 -0300

    Update index.php

--- wp-content/themes/tema-dani-terapeuta/index.php
+++ wp-content/themes/tema-dani-terapeuta/index.php
@@ -1,6 +1,30 @@
 <?php get_header(); ?>
-    <div class="container">
-        <h1 class="text-center text-primary">Página Inicial</h1>
+<nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container">
+  <a class="navbar-brand" href="#section-1"><span class="titulo">Danielle Zagonel Machado</span></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#section-1">Sobre</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#section-2">Psicoterapias</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#section-3">Contato</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Blog</a>
+        </li>
+      </ul>
+     
     </div>
+  </div>
+</nav>
     
+<?php get_template_part('home-sections'); ?>
+
 <?php get_footer(); ?>
