34b8fedd5eca41b3e3aabddaa8acc69b435a6d1e
Marcelo <marcelo_levek@hotmail.com>
2023-01-30 09:33:22 -0300

    Update home-sections.php

--- wp-content/themes/tema-dani-terapeuta/home-sections.php
+++ wp-content/themes/tema-dani-terapeuta/home-sections.php
@@ -6,7 +6,7 @@
     <div class="container">
    <div class="row">
     <div class="col-sm-12 col-md-6 mt-5">
-        <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/foto-dani-circular.png" alt='foto Danielle Zagonel Machado' class="img-fluid foto-dani">
+        <img src="https://dani-terapias.000webhostapp.com/wp-content/uploads/2023/01/foto-dani-circular.jpg" alt='foto Danielle Zagonel Machado' class="img-fluid foto-dani">
     </div>
     <div class="col-sm-12 col-md-6 mt-5">
         <p class="fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, quidem quod. Velit, delectus officia. Voluptate adipisci quaerat laboriosam voluptatem ea repudiandae aspernatur, qui vel doloribus at quisquam, iste assumenda quia!</p> 
@@ -26,7 +26,7 @@
 <div class="row row-cols-1 row-cols-md-2 g-4 mt-4">
   <div class="col">
     <div class="card">
-      <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/teclando-no-notebook.png" class="card-img-top" alt="imagem pessoa teclando no notebook">
+      <img src="https://dani-terapias.000webhostapp.com/wp-content/uploads/2023/01/teclando-no-notebook.png" class="card-img-top" alt="imagem pessoa teclando no notebook">
       <div class="card-body">
         <h5 class="card-title">Psicoterapia à distância</h5>
         <p class="card-text">"Crescimento pessoal e autoconhecimento: a jornada da terapia"</p>
@@ -35,7 +35,7 @@
   </div>
   <div class="col">
     <div class="card">
-      <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/imagem-pessoa-feliz.png" class="card-img-top" alt="imagem pessoa feliz">
+      <img src="https://dani-terapias.000webhostapp.com/wp-content/uploads/2023/01/imagem-pessoa-feliz.png" class="card-img-top" alt="imagem pessoa feliz">
       <div class="card-body">
         <h5 class="card-title">Psicoterapia para adultos</h5>
         <p class="card-text">"Sinta-se melhor: terapia para melhorar sua qualidade de vida"</p>
@@ -44,7 +44,7 @@
   </div>
   <div class="col">
     <div class="card">
-      <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/imagem-criancas-e-adolescentes.png" class="card-img-top" alt="imagem crianças e adolescentes sentados na grama">
+      <img src="https://dani-terapias.000webhostapp.com/wp-content/uploads/2023/01/imagem-criancas-e-adolescentes.png" class="card-img-top" alt="imagem crianças e adolescentes sentados na grama">
       <div class="card-body">
         <h5 class="card-title">Psicoterapia para crianças e adolescentes</h5>
         <p class="card-text">"Construindo uma vida mais plena: terapia para alcançar metas e realizações".</p>
@@ -53,7 +53,7 @@
   </div>
   <div class="col">
     <div class="card">
-      <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/imagem-pais-e-filho.png" class="card-img-top" alt="imagem pais e filho ao por do sol">
+      <img src="https://dani-terapias.000webhostapp.com/wp-content/uploads/2023/01/imagem-pais-e-filho.png" class="card-img-top" alt="imagem pais e filho ao por do sol">
       <div class="card-body">
         <h5 class="card-title">Aconselhamento parental</h5>
         <p class="card-text">"Reconstruindo relacionamentos: terapia de casal e familiar"</p>
@@ -74,9 +74,9 @@ dynamic_sidebar('contato'); ?>
 </div> 
   <div class="col-sm-12 col-md-6 mt-5">
     <ul class="contato">
-      <li><a href="" class="links-contato"><img src="http://localhost/dani-psico/wp-content/uploads/2023/01/image-email.png" target="_blank" alt="email Danielle Machado"> E-mail</a>  </li>
-      <li><a href="https://www.facebook.com/espacogestalt" target="_blank" class="links-contato"> <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/image-facebook.png" alt="facebook Danielle Machado"> Facebook</a></li>
-      <li><a href="https://wa.me/5541988416624?text=sua mensagem" target="_blank" class="links-contato"> <img src="http://localhost/dani-psico/wp-content/uploads/2023/01/image-whatsapp.png" alt="whatsapp Danielle Machado"> Whatsapp</a></li>    
+      <li><a href="" class="links-contato"><img src="https://dani-terapias.000webhostapp.com/wp-content/uploads/2023/01/image-email.png" target="_blank" alt="email Danielle Machado"> E-mail</a>  </li>
+      <li><a href="https://www.facebook.com/espacogestalt" target="_blank" class="links-contato"> <img src="https://dani-terapias.000webhostapp.com/wp-content/uploads/2023/01/image-facebook.png" alt="facebook Danielle Machado"> Facebook</a></li>
+      <li><a href="https://wa.me/5541988416624?text=sua mensagem" target="_blank" class="links-contato"> <img src="https://dani-terapias.000webhostapp.com/wp-content/uploads/2023/01/image-whatsapp.png" alt="whatsapp Danielle Machado"> Whatsapp</a></li>    
   </ul>
   </div>
 </div>    
